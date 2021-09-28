f9238a1c1103e8520f1268f420c299131879c40b
NATÃ SATO RODRIGUES <20272585@cfp127aluno.local>
2021-09-28 11:17:08 -0300

    Segunda tela feita

--- app/src/main/java/br/com/kingofservice/MainActivity.kt
+++ app/src/main/java/br/com/kingofservice/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.kingofservice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bt_login.setOnClickListener {
+            IrParaTelaLogin()
+        }
+    }
+
+    private fun IrParaTelaLogin(){
+
+        val telaLogin = Intent(this,TelaLogin::class.java)
+        startActivity(telaLogin)
     }
 }
