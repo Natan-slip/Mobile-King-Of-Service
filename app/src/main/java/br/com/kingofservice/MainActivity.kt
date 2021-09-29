dbbb7e395cbedbd88aca915edf9337e76cc095b9
NATÃ SATO RODRIGUES <20272585@cfp127aluno.local>
2021-09-29 11:23:23 -0300

    Tela de Cadastro concluída

--- app/src/main/java/br/com/kingofservice/MainActivity.kt
+++ app/src/main/java/br/com/kingofservice/MainActivity.kt
@@ -3,20 +3,21 @@ package br.com.kingofservice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        btt_login.setOnClickListener {
-//            IrParaTelaLogin()
-//        }
-//    }
+        btt_login.setOnClickListener {
+            IrParaTelaLogin()
+        }
+    }
 
-//    private fun IrParaTelaLogin(){
-//
-//        val telaLogin = Intent(this,TelaLogin::class.java)
-//        startActivity(telaLogin)
+    private fun IrParaTelaLogin(){
+
+        val telaLogin = Intent(this,TelaLogin::class.java)
+        startActivity(telaLogin)
     }
 }
